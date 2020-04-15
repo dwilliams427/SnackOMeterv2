1a6caf4f60706277ed22cc0d69b7415b48e8c71c
Daniel Williams <dlw1l27@gmail.com>
2020-04-14 17:03:48 -0700

    Implemented SHARED PREFERENCES to save meal and snack counter For implenting the shared preferences, checkout this video: https://www.youtube.com/watch?v=fJEFZ6EOM9o It is for java but use the boiler plate code here

--- app/src/main/java/com/danielwilliams/snackometerv2/MainActivity.kt
+++ app/src/main/java/com/danielwilliams/snackometerv2/MainActivity.kt
@@ -1,10 +1,15 @@
 package com.danielwilliams.snackometerv2
 
+/*
+For implenting the shared preferences, checkout this video: https://www.youtube.com/watch?v=fJEFZ6EOM9o
+It is for java but use the boiler plate code here
+ */
+
+
 import android.content.Context
 import android.content.SharedPreferences
 import android.content.SharedPreferences.Editor
 import android.os.Bundle
-import android.preference.PreferenceManager
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -15,9 +20,16 @@ class MainActivity() : AppCompatActivity() {
     var mealCounter = 0
     var snackCounter = 0
     val CUSTOM_PREF_NAME = "Meal Counter"
+    val MEAL_TEXT = "meal counter"
+    val SNACK_TEXT = "snack counter"
+    var mealText : String = ""
+    var snackText : String = ""
+
+
+
 
     //setup shared preferences
-//    private val prefs: SharedPreferences =  getSharedPreferences("MEAL_PREFS", Context.MODE_PRIVATE)
+//    private val prefs: SharedPreferences =  getSharedPreferences(CUSTOM_PREF_NAME, Context.MODE_PRIVATE)
 //    var editor = prefs.edit()
 
 
@@ -32,10 +44,11 @@ class MainActivity() : AppCompatActivity() {
         mealPlusButton.setOnClickListener{
             mealVal.setText("" + ++mealCounter)
 
+            saveData()
             // setup shared pref
-//            editor.putString("MEAL_COUNTER", mealVal.text.toString())
+//            editor.putString("MEAL_TEXT", mealVal.text.toString())
 //            editor.commit()
-            println("saved meal plus counter")
+//            println("saved meal plus counter")
 
         }
         mealMinusButton.setOnClickListener{
@@ -43,29 +56,73 @@ class MainActivity() : AppCompatActivity() {
                 mealVal.setText("0")
             }
             else mealVal.setText("" + --mealCounter)
-
-
-
+            saveData()
         }
 
         //setup snack counter
         snackVal.setText("" + snackCounter)
         snackPlusButton.setOnClickListener {
             snackVal.setText("" + ++snackCounter)
+            saveData()
         }
         snackMinusButton.setOnClickListener{
             if (snackCounter <= 0){
                 snackVal.setText("0")
             }
             else snackVal.setText("" + --snackCounter)
+            saveData()
         }
 
-
+        loadData()
+        updateViews()
     }
 
-//    fun onClick(v: View?){
-//        val prefs = customPreference(this, CUSTOM_PREF_NAME)
-//    }
+
+    fun saveData(){
+        val prefs: SharedPreferences =  getSharedPreferences(CUSTOM_PREF_NAME, Context.MODE_PRIVATE)
+        var editor = prefs.edit()
+
+        editor.putString(MEAL_TEXT, mealVal.text.toString())
+        editor.putString(SNACK_TEXT, snackVal.text.toString())
+        editor.apply()
+
+        Toast.makeText(applicationContext,"data saved",Toast.LENGTH_SHORT).show()
+    }
+
+    fun loadData(){
+        val prefs: SharedPreferences =  getSharedPreferences(CUSTOM_PREF_NAME, Context.MODE_PRIVATE)
+        mealText = prefs.getString(MEAL_TEXT, "").toString()
+        snackText = prefs.getString(SNACK_TEXT, "").toString()
+    }
+
+    fun updateViews(){
+        mealVal.setText(mealText)
+        snackVal.setText(snackText)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        loadData()
+        Toast.makeText(applicationContext,"Resumed",Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        loadData()
+        Toast.makeText(applicationContext,"Restarted",Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        saveData()
+        Toast.makeText(applicationContext,"Paused",Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        saveData()
+        Toast.makeText(applicationContext,"fuckin DESTROYED",Toast.LENGTH_SHORT).show()
+    }
 
 //    override fun onPause() {
 //        super.onPause()
